324e8ff90b02da20aead49ec1fd376f923c1c361
dinesh-g1 <dineshgillalaa@gmail.com>
2025-03-04 11:42:46 +0530

    feat: configuring the logging

--- src/main/java/com/gdinesh/taskmanager/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/gdinesh/taskmanager/service/impl/ProjectServiceImpl.java
@@ -2,6 +2,8 @@ package com.gdinesh.taskmanager.service.impl;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -12,16 +14,19 @@ import com.gdinesh.taskmanager.service.IProjectService;
 
 @Service
 public class ProjectServiceImpl implements IProjectService {
-    
+    private static final Logger LOG = LoggerFactory.getLogger(ProjectServiceImpl.class);
+
     IProjectRepository projectRepo;
 
     @Override
     public Optional<Project> findById(Long id) {
+        LOG.debug("Found project by Id >> {} ", id);
         return projectRepo.findById(id);
     }
 
     @Override
     public Project save(Project project) {
+        LOG.debug("Saving Project >> {} ", project);
         return projectRepo.save(project);
     }
     
