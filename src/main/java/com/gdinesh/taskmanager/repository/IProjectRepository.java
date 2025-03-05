2ae47a8e7eeb41e9e9696a8936a06563b906e06b
dinesh-g1 <dineshgillalaa@gmail.com>
2025-03-05 18:54:12 +0530

    feat: using default jpa capabilities.

--- src/main/java/com/gdinesh/taskmanager/repository/IProjectRepository.java
+++ src/main/java/com/gdinesh/taskmanager/repository/IProjectRepository.java
@@ -1,9 +1,17 @@
 package com.gdinesh.taskmanager.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gdinesh.taskmanager.persistence.model.Project;
 
+
 @Repository
-public interface IProjectRepository extends CrudRepository<Project, Long> {}
+public interface IProjectRepository extends CrudRepository<Project, Long> {
+    Optional<Project> findByName(String name);
+    List<Project> findByCreatedAtBetween(LocalDate start, LocalDate end);
+}
--- src/test/java/com/gdinesh/taskmanager/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/gdinesh/taskmanager/ProjectRepositoryIntegrationTest.java
@@ -1,11 +1,13 @@
 package com.gdinesh.taskmanager;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -29,11 +31,36 @@ public class ProjectRepositoryIntegrationTest {
     }
 
     @Test
-    public void givenId_retrieveProject_thenSuccess() {
+    public void givenId_whenFindById_thenSuccess() {
         Project p = new Project(1L, "test project 1", LocalDate.now());
         projectRepository.save(p);
 
         Optional<Project> retrievedProject = projectRepository.findById(p.getId());
         assertEquals(p.getId(), retrievedProject.get().getId());
+    }
+
+    @Test
+    public void givenProjectName_whenFindByName_thenSuccess() {
+        Project p = new Project(1L, "test project 1", LocalDate.now());
+        projectRepository.save(p);
+
+        Optional<Project> retrievedProject = projectRepository.findByName(p.getName());
+        assertEquals(p, retrievedProject.get());
+    }
+
+    @Test
+    public void givenProjectCreatedAt_whenFindByCreatedAtBetween_thenSuccess() {
+        Project p = new Project(1L, "test project 1", LocalDate.now());
+        projectRepository.save(p);
+
+        Project p1 = new Project(2L, "test project 2", LocalDate.now());
+        projectRepository.save(p1);
+
+        Project p3 = new Project(3L, "old project 3", LocalDate.now().minusYears(1));
+        projectRepository.save(p3);
+
+        List<Project> retrievedProjects = projectRepository.findByCreatedAtBetween(LocalDate.now().minusDays(1L), LocalDate.now().plusDays(1L));
+        
+        assertThat(retrievedProjects, hasItems(p, p1));
     }
 }
